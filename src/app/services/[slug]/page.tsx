135bdde0e582c928e8a12bc8e0b9c53399c3d432
Tamojit Mandal <tamojitm23@gmail.com>
2025-06-08 20:47:24 +0530

    Fix: Buy services

--- src/app/services/[slug]/page.tsx
+++ src/app/services/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { services } from "@/data/services";
 import { Button } from "@/components/ui/button";
 import React from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 type Props = {
     params: Promise<{
@@ -18,7 +19,7 @@ export default async function ServiceDetail(props: Props) {
 
     return (
         <div className="text-white pt-12">
-            <h1 className="text-4xl font-bold p-5 ">{service.title}</h1>
+            <h1 className="text-5xl font-bold p-5 mx-15 ">{service.title}</h1>
             <div className="flex flex-row p-10 pl-20 pr-10">
                 <Image
                     src={service.image}
@@ -28,7 +29,6 @@ export default async function ServiceDetail(props: Props) {
                     height={120}
                 />
                 <div className="pt-5 pl-15 max-w-2xl">
-                    <p>Details about {service.title} go here...</p>
                     <p>{service.description}</p>
                     <p className="max-w-78 mt-5">{service.features}</p>
                     <div className="pt-4 flex flex-row m-5 space-x-10">
@@ -38,14 +38,17 @@ export default async function ServiceDetail(props: Props) {
                         >
                             {service.price}
                         </Button>
-                        <Button
-                            variant="outline"
-                            className=" bg-red-600 text-white text-xl hover:bg-white hover:text-red-600"
-                        >
-                            BUY SERVICE
-                        </Button>
+                        <Link href={"https://t.me/your_channel"}>
+                            <Button
+                                variant="outline"
+                                className=" bg-red-600 text-white text-xl hover:bg-white hover:text-red-600"
+                            >
+                                BUY SERVICE
+                            </Button>
+                        </Link>
+
                     </div>
-                    
+
                 </div>
             </div>
         </div>
